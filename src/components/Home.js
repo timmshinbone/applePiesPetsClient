7c9daa740902cd4944203571221ab1f4e1c47486
Timm Schoenborn <timmschoenborn@gmail.com>
2024-02-05 13:47:01 -0500

    adds loading screen and api call useEffect notes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react'
 import { getAllPets } from "../api/pet"
+import LoadingScreen from './shared/LoadingScreen'
 
 const Home = (props) => {
 	const { msgAlert, user } = props
@@ -11,15 +12,29 @@ const Home = (props) => {
 	// the first is a callback function
 	// the second arg is a dependency array
 	// the dependency array, tells react when to run the effect hook. If we want this to run only on the first render and anytime the page refreshes, we keep the dependency array empty
+	// useEffect is called RIGHT after the FIRST render of the component
 	useEffect(() => {
 		getAllPets()
-			.then(res => console.log('pets: \n', res.data.pets))
+			// .then(res => console.log('pets from axios call: \n', res.data.pets))
+			.then(res => {
+				console.log('use Effect hook ran')
+				setPets(res.data.pets)
+			})
 			.catch(error => console.error)
 	}, [])
 
+	// WE NEVER EVER DO THIS:
+	// getAllPets()
+	// 	.then(res => setPets(res.data.pets))
+	// 	.catch(err => console.log(err))
+	// API calls need to happen in an effect hook, or as the result of a singular action
+	// (like a form submission)
+	console.log('the pets in Home: \n', pets)
 	return (
 		<>
 			<h2>Home Page</h2>
+			{ user !== null ? <h5>Hello {user.email}</h5> : null }
+			{pets == null ? <LoadingScreen /> : <p>{pets[0].name}</p>}
 		</>
 	)
 }
