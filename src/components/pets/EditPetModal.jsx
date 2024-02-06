e38c0daecf26e492286c28242c7d1bf58c72bbb2
Timm Schoenborn <timmschoenborn@gmail.com>
2024-02-06 11:31:48 -0500

    adds the beginnings of the update modal

---
+++ src/components/pets/EditPetModal.jsx
@@ -0,0 +1,33 @@
+// this modal is rendered by the PetShow component
+// the state that controls this modal (show) will live in the PetShow component(the parent of this modal)
+// the state, as well as the updater function for that state, will be passed to this modal as props
+// other props that we will need, are  the user, updatePet, msgAlert, and triggerRefresh
+
+import React, { useState } from 'react'
+import { Modal } from 'react-bootstrap'
+import PetForm from '../shared/PetForm'
+import messages from '../shared/AutoDismissAlert/messages'
+
+const EditPetModal = (props) => {
+    // pull the important things from our props
+    const { user, show, handleClose, updatePet, msgAlert, triggerRefresh } = props
+    // we're bringing in the pet from props, but only for the initial state
+    // by using the original pet as our initial state for a NEW piece of state, specific to this component (called pet), we'll be able to modify the pet we are updating without affecting the original state in the parent component
+    const [pet, setPet] = useState(props.pet)
+
+    return (
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton />
+            <Modal.Body>
+                <PetForm 
+                    pet={pet}
+                    handleChange={onChange}
+                    handleSubmit={onSubmit}
+                    heading="Update Pet"
+                />
+            </Modal.Body>
+        </Modal>
+    )
+}
+
+export default EditPetModal
