6444cd3c25093d5191969c901a0f8972648a0d23
Timm Schoenborn <timmschoenborn@gmail.com>
2024-02-06 16:24:23 -0500

    adds style to toyCards

--- src/components/toys/ToyShow.jsx
+++ src/components/toys/ToyShow.jsx
@@ -6,12 +6,23 @@ const ToyShow = (props) => {
     // for the first iteration of this component, we'll only need one prop - the toy
     const { toy } = props
 
+    const setBgCondition = (cond) => {
+        // a toy can either be new, used, or disgusting
+        if (cond === 'new') {
+            return ({width: '18rem', backgroundColor: '#b5ead7'})
+        } else if (cond === 'used') {
+            return ({width: '18rem', backgroundColor: '#ffdac1'})
+        } else {
+            return ({width: '18rem', backgroundColor: '#ff9aa2'})
+        }
+    }
+
     return (
         <>
-            <Card className='m-2'>
+            <Card className='m-2' style={setBgCondition(toy.condition)}>
                 <Card.Header>{toy.name}</Card.Header>
                 <Card.Body>
-                    <small>{toy.description}</small>
+                    <small>{toy.description}</small><br/>
                     <small>{toy.isSqueaky ? 'squeak squeak' : 'stoic silence'}</small>
                 </Card.Body>
                 <Card.Footer>
